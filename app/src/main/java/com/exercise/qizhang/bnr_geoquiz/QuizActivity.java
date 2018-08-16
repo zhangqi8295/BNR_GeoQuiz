d711b7538c9b87616098ecab77b302cf978585d9
Qi Zhang <zhangqi2062@students.itu.edu>
2018-08-16 10:40:34 -0700

    Finish Chapter 1 content

--- app/src/main/java/com/exercise/qizhang/bnr_geoquiz/QuizActivity.java
+++ app/src/main/java/com/exercise/qizhang/bnr_geoquiz/QuizActivity.java
@@ -2,12 +2,38 @@ package com.exercise.qizhang.bnr_geoquiz;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class QuizActivity extends AppCompatActivity {
+    private Button mTrueButton;
+    private Button mFalseButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
+
+        initWidgets();
+
+        mTrueButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(QuizActivity.this, R.string.correct_toast, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        mFalseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(QuizActivity.this, R.string.incorrect_toast, Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void initWidgets() {
+        mTrueButton = findViewById(R.id.true_button);
+        mFalseButton = findViewById(R.id.false_button);
     }
 }
